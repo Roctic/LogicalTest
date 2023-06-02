b75f31231407eb2734c86b706f5f1745358f0cb8
V <104692696+Roctic@users.noreply.github.com>
2023-06-02 16:25:20 -0300

    Add files via upload

---
+++ LogicalTest/src/main/java/com/mycompany/logicaltest/LogicalTest.java
@@ -0,0 +1,29 @@
+package com.mycompany.logicaltest;
+
+/**
+ *
+ * @author Roctic
+ */
+public class LogicalTest {
+
+    public static void main(String[] args) {
+        int age = 17;
+//      Decremento --
+
+        for (int i = 10; i >= 1;i--){
+            System.out.println("oi"+ i);
+        }
+        
+        
+//         Incremento ++        
+//        for (int i = 0; i <= 10; i++){
+//            System.out.println("oi"+ i);
+//        }
+        
+        
+//       System.out.println("Result: " + (age < 18? false : true));
+//       age < 18? false : true;
+        
+        
+    }
+}
